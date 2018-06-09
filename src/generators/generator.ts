6dae97ead8eb3f67c4212e22a73ce29053daae88
Aler Denisov <aler.zampillo@gmail.com>
2018-06-09 11:58:51 +0800

    generator draft

--- src/generators/generator.ts
+++ src/generators/generator.ts
@@ -1,3 +1,6 @@
+import * as path from "path";
+import * as globby from "globby";
+
 export interface GeneratorOptions {
   quiet: boolean;
   force: boolean;
@@ -14,6 +17,50 @@ export default abstract class Generator<T extends object>
   abstract readonly defaultConfig: T;
 
   async generate(opts: GeneratorOptions & T) {
-    // do something
+    this.options = opts;
+    this.options = Object.assign(this.defaultConfig, opts);
+    await this.configure();
+
+    if (!this.options.quiet) {
+      this.options = Object.assign(this.options, await this.prompt());
+    }
+
+    await this.validate();
+    await this.install();
+  }
+
+  // placeholders to override in inherit classes
+  async configure() {}
+  async validate() {}
+
+  abstract async install(): Promise<void>;
+  abstract async prompt(): Promise<T>;
+
+  async clone(template: string, ...destination: string[]): Promise<void> {
+    const destinationPath = path.resolve(
+      path.resolve(this.options.cwd, ...destination)
+    );
+    const templatePath = path.resolve(path.join(this.templateRoot, template));
+    await fs.copy(templatePath, destinationPath, {
+      recursive: true
+    });
+  }
+
+  async prepare(...relative: string[]): Promise<void> {
+    const templates = await globby("**/*.ejs", {
+      absolute: true,
+      cwd: relative.length
+        ? path.resolve(this.options.cwd, ...relative)
+        : this.options.cwd
+    });
+
+    await Promise.all(templates.map(file => this.prepareFile(file)));
+  }
+
+  async prepareFile(file: string): Promise<void> {
+    const fileContent = await fs.readFile(file, "utf8");
+    const preparedFile = hb.compile(fileContent)(this.options);
+    await fs.writeFile(file, preparedFile);
+    await fs.rename(file, hb.compile(file.replace(".ejs", ""))(this.options));
   }
 }
--- src/generators/project-generator.ts
+++ src/generators/project-generator.ts
@@ -1,3 +1,4 @@
+import * as path from "path";
 import Generator from "./generator";
 
 export interface ProjectGeneratorConfig {
@@ -7,5 +8,24 @@ export interface ProjectGeneratorConfig {
 }
 
 export class ProjectGenerator extends Generator<ProjectGeneratorConfig> {
-  defaultConfig: ProjectGeneratorConfig;
+  get defaultConfig(): ProjectGeneratorConfig {
+    const slugName = path.basename(this.options.cwd);
+    return {
+      name: slugName,
+      description: "Generic EOSIC project",
+      withBuiltin: true
+    };
+  }
+
+  async install(): Promise<void> {
+    await this.clone("project", ".");
+    if (this.options.withBuiltin) {
+      await this.clone("builtin", ".", "builtin");
+    }
+    await this.prepare();
+  }
+
+  async prompt(): Promise<ProjectGeneratorConfig> {
+    throw new Error("Method not implemented.");
+  }
 }
